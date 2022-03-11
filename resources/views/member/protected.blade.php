58a8b59fd97ca761e0073764778b08dff6d0b00c
html_debojyti <debojyti@keylines.net>
2022-03-11 16:04:36 +0530

    member, member image

--- resources/views/member/protected.blade.php
+++ resources/views/member/protected.blade.php
@@ -2,16 +2,39 @@
 @section('content')
 <div class="col-lg-9 col-md-7 p-0">
     <div class="right-body">
-<section class="member-protected">
-<div class="container">
-    <div class="row">
-        @if(Session::has('fail'))
-            {{ Session::get('fail') }}
-        @endif
-    </div>
-</div>
-</section>
-</div>
-</div>
+        <section class="banner">
+            <div class="banner-box">
+                <div id="innerpage-banner" class="owl-carousel owl-theme">
+                    <div class="item">
+                        <div class="about-img">
+                            <img class="img-fluid" src="{{ asset('img/past-president/banner1.jpg') }}" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
 
+
+        <section class="member-protected">
+            
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    @if(Session::has('fail'))
+                        <div class="membermustlogin_box">
+                            <div class="membermustlogin_img"><img class="img-fluid" src="{{ asset('img/alerticon_memb.png') }}" alt="" /></div> 
+                            <div class="menbt_text">{{ Session::get('fail') }}</div>
+
+                        </div>
+                    @endif
+                </div>
+            </div>
+        </div>
+        </section>
+    
+
+@include('common.footer')
+</body>
+
+</html>
 @endsection
