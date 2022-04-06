92d1b0a256f58569113f3ca33bd5fc8ae691a4a7
pranoy <pranoy@keyline.net>
2022-04-06 10:15:23 +0530

    admin panel menu page is update

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -6,9 +6,11 @@
 
     <!-- Sidebar -->
     <div class="sidebar">
+
         <!-- Sidebar user (optional) -->
 
         <!-- Sidebar Menu -->
+
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <li class="nav-item">
