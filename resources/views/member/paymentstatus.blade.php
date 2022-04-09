46a4c866447d1f6d26a2ae6ba4ceedcad30994ac
Shuvadeep Chakraborty <shuvadeep@keylines.net>
2022-04-09 17:24:53 +0530

    issue in blade after payment

--- resources/views/member/paymentstatus.blade.php
+++ resources/views/member/paymentstatus.blade.php
@@ -52,7 +52,7 @@
                                         <h4>Payment Status : {{ $status['status'] }}</h4>
                                         <h4>Payment Ref. No.: {{ $status['mihpayid'] }}</h4>
                                     </div>
-                                    @if ($status['status'] == 'successful')
+                                    @if ($status['status'] == 'success')
                                     <span class="success-msg">Thank you for your payment. The paid amount will reflect in your account within next 24 hours.</span>
                                     @else
                                     <a class="btn btn-info" href="{{ route('member.invoice')}}">Try Again</a>
