4ccd31e6b091e02cd58fbadf23623669c1d9fb91
pranoy <pranoy@keyline.net>
2022-05-06 11:01:26 +0530

    newsletter page is update

--- resources/frontend/1792-newsletter.blade.php
+++ resources/frontend/1792-newsletter.blade.php
@@ -86,12 +86,16 @@
                                 @foreach($contentPages->where("id","18") as $contentPage)
                                 {!! $contentPage->page_text !!}
                                 @endforeach
+
+
                                 <!-- <p>In the city of Calcutta, then just over a hundred years old and growing fast both in
                                     commercial and political significance, the British Raj was busy setting its roots.
                                     And sports were definitely a part of the social lore.</p>
                                 <p>The club also offers food from its different counters like charcoal-grilled kebabs,
                                     quick bites of wraps, burgers, pastas etc. There is also a pastry shop and
                                     specialized tea & coffee counters serving wide varieties of tea and coffee.</p> -->
+
+
                             </div>
                         </div>
                     </div>
