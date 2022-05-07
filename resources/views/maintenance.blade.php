55610df18eb77e096cf71697c9abd889c28cf602
Shuvadeep Chakraborty <shuvadeep@keylines.net>
2022-05-07 11:12:50 +0530

    maintanance mode

--- resources/views/maintenance.blade.php
+++ resources/views/maintenance.blade.php
@@ -1,4 +1,12 @@
-<div>
-    <img class="img-fluid" src="{{URL::to('/img/email-logo.png')}}" alt="" />
-    <p>Coming soon...</p>
-</div>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- <title>This is test mail</title> -->
+</head>
+<body>
+<div style="margin: 0 auto; display:table;"><a href="{{ url()->current() }}"><img class="img-fluid" src="{{URL::to('/img/email-logo.png')}}" alt="" /></a></div>
+<div style="font-weight: 600; font-size:20px; text-align: center; font-family:Arial, Helvetica, sans-serif;">New CCFC website coming soon!</div>
+</body>
+</html>
