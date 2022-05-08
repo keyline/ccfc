66d8c6b0163a4df1c6731a3c9238d9da5b312407
pranoy <pranoy@keyline.net>
2022-05-08 22:09:18 +0530

    sports part is update

--- resources/views/admin/reciprocalClubs/index.blade.php
+++ resources/views/admin/reciprocalClubs/index.blade.php
@@ -107,13 +107,15 @@
                             </a>
                             @endcan
 
-                            <!-- @can('reciprocal_club_delete')
-                                    <form action="{{ route('admin.reciprocal-clubs.destroy', $reciprocalClub->id) }}" method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
-                                        <input type="hidden" name="_method" value="DELETE">
-                                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                        <input type="submit" class="btn btn-xs btn-danger" value="{{ trans('global.delete') }}">
-                                    </form>
-                                @endcan -->
+                            @can('reciprocal_club_delete')
+                            <form action="{{ route('admin.reciprocal-clubs.destroy', $reciprocalClub->id) }}"
+                                method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');"
+                                style="display: inline-block;">
+                                <input type="hidden" name="_method" value="DELETE">
+                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                <input type="submit" class="btn btn-xs btn-danger" value="{{ trans('global.delete') }}">
+                            </form>
+                            @endcan
 
                         </td>
 
