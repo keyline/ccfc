f4c2712bc8dee0012d1531ebeaa486364231aa4c
Shuvadeep Chakraborty <shuvadeep@keylines.net>
2024-02-05 16:22:08 +0530

    setup at hourly

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('update:users')->dailyAt('4:00'); // 4 A.M
+        //$schedule->command('update:users')->dailyAt('4:00'); // 4 A.M
+        $schedule->command('update:users')->hourly();
 
     }
 
