7eaf80ca8cc308ff6452c35d4e15018014f766d9
subhomoy samanta <subhomoy@keylines.net>
2024-06-17 14:19:37 +0530

    api environment setup

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -1,5 +1,5 @@
 <?php
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
@@ -21,27 +21,27 @@ class ApiController extends Controller
         $apiExtraData       = '';
         $requestData        = $request->all();
         Helper::pr($requestData);
-        if($requestData['key'] == env('PROJECT_KEY')){
-            $id         = $requestData['id'];
-            $otp        = $requestData['otp1'].$requestData['otp2'].$requestData['otp3'].$requestData['otp4'];
-            $getUser    = User::where('id', '=', $id)->first();
-            if($getUser){
+        // if($requestData['key'] == env('PROJECT_KEY')){
+        //     $id         = $requestData['id'];
+        //     $otp        = $requestData['otp1'].$requestData['otp2'].$requestData['otp3'].$requestData['otp4'];
+        //     $getUser    = User::where('id', '=', $id)->first();
+        //     if($getUser){
                 
-            } else {
-                $apiStatus          = FALSE;
-                http_response_code(400);
-                $apiMessage         = 'User Not Found !!!';
-                $apiExtraField      = 'response_code';
-                $apiExtraData       = http_response_code();
-            }
-        } else {
-            http_response_code(400);
-            $apiStatus          = FALSE;
-            $apiMessage         = $this->getResponseCode(http_response_code());
-            $apiExtraField      = 'response_code';
-            $apiExtraData       = http_response_code();
-        }
-        $this->response_to_json($apiStatus, $apiMessage, $apiResponse, $apiExtraField, $apiExtraData);
+        //     } else {
+        //         $apiStatus          = FALSE;
+        //         http_response_code(400);
+        //         $apiMessage         = 'User Not Found !!!';
+        //         $apiExtraField      = 'response_code';
+        //         $apiExtraData       = http_response_code();
+        //     }
+        // } else {
+        //     http_response_code(400);
+        //     $apiStatus          = FALSE;
+        //     $apiMessage         = $this->getResponseCode(http_response_code());
+        //     $apiExtraField      = 'response_code';
+        //     $apiExtraData       = http_response_code();
+        // }
+        // $this->response_to_json($apiStatus, $apiMessage, $apiResponse, $apiExtraField, $apiExtraData);
     }
     
 }
