79686f468645fc06159e67bae0da3f7d9b7c89c0
subhomoy samanta <subhomoy@keylines.net>
2024-06-17 13:03:52 +0530

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
         <div class="col-lg-12">
             <div class="card">
                 <div class="card-header">
-                    Dashboard
+                    Dashboard v2
                 </div>
 
                 <div class="card-body">
