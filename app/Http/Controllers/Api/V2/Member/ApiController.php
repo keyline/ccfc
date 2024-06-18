e986ce28e8cf9ddfb90918a37e90b72fdd20616d
subhomoy samanta <subhomoy@keylines.net>
2024-06-18 13:11:22 +0530

    api environment setup

--- app/Http/Controllers/Api/V2/Member/ApiController.php
+++ app/Http/Controllers/Api/V2/Member/ApiController.php
@@ -29,7 +29,7 @@ class ApiController extends Controller
                 $apiStatus          = FALSE;
                 $apiMessage         = 'All Data Are Not Present !!!';
             }
-            // echo $headerData['Key'][0];
+            echo $headerData['key'][0];
             // Helper::pr($headerData['Key']);
             echo '<br>';
             Helper::pr($headerData);
