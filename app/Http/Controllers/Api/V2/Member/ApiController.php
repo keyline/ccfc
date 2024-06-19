25f05fb84cc2001255d71bb95be49011ca40e3a5
subhomoy samanta <subhomoy@keylines.net>
2024-06-19 11:51:20 +0530

    api implemented on 19-06-2024 dashboard,get profile,my card,contact us

--- app/Http/Controllers/Api/V2/Member/ApiController.php
+++ app/Http/Controllers/Api/V2/Member/ApiController.php
@@ -453,9 +453,10 @@ class ApiController extends Controller
                 $apiExtraField      = '';
                 $apiExtraData       = '';
                 $headerData         = $request->header();
-                Helper::pr($headerData);
                 if($headerData['key'][0] == env('PROJECT_KEY')){
-                    $id                         = $requestData['id'];
+                    $app_access_token           = $headerData['authorization'][0];
+                    $getTokenValue              = $this->tokenAuth($app_access_token);
+                    Helper::pr($getTokenValue);
                     $checkUser                  = User::where('id', '=', $id)->first();
                     if($checkUser){
                         if($checkUser->status == 'ACTIVE'){
@@ -596,4 +597,142 @@ class ApiController extends Controller
             }
         /* post contact us */
     /* after login */
+    /*
+    Get http response code
+    Author : Subhomoy
+    */
+    private function getResponseCode($code = NULL){
+        if ($code !== NULL) {
+            switch ($code) {
+                case 100: $text = 'Continue'; break;
+                case 101: $text = 'Switching Protocols'; break;
+                case 200: $text = 'OK'; break;
+                case 201: $text = 'Created'; break;
+                case 202: $text = 'Accepted'; break;
+                case 203: $text = 'Non-Authoritative Information'; break;
+                case 204: $text = 'No Content'; break;
+                case 205: $text = 'Reset Content'; break;
+                case 206: $text = 'Partial Content'; break;
+                case 300: $text = 'Multiple Choices'; break;
+                case 301: $text = 'Moved Permanently'; break;
+                case 302: $text = 'Moved Temporarily'; break;
+                case 303: $text = 'See Other'; break;
+                case 304: $text = 'Not Modified'; break;
+                case 305: $text = 'Use Proxy'; break;
+                case 400: $text = 'Unauthenticated Request !!!'; break;
+                case 401: $text = 'Token Not Found !!!'; break;
+                case 402: $text = 'Payment Required'; break;
+                case 403: $text = 'Token Has Expired !!!'; break;
+                case 404: $text = 'User Not Found !!!'; break;
+                case 405: $text = 'Method Not Allowed'; break;
+                case 406: $text = 'All Data Are Not Present !!!'; break;
+                case 407: $text = 'Proxy Authentication Required'; break;
+                case 408: $text = 'Request Time-out'; break;
+                case 409: $text = 'Conflict'; break;
+                case 410: $text = 'Gone'; break;
+                case 411: $text = 'Length Required'; break;
+                case 412: $text = 'Precondition Failed'; break;
+                case 413: $text = 'Request Entity Too Large'; break;
+                case 414: $text = 'Request-URI Too Large'; break;
+                case 415: $text = 'Unsupported Media Type'; break;
+                case 500: $text = 'Internal Server Error'; break;
+                case 501: $text = 'Not Implemented'; break;
+                case 502: $text = 'Bad Gateway'; break;
+                case 503: $text = 'Service Unavailable'; break;
+                case 504: $text = 'Gateway Time-out'; break;
+                case 505: $text = 'HTTP Version not supported'; break;
+                default:
+                    exit('Unknown http status code "' . htmlentities($code) . '"');
+                break;
+            }
+            $protocol = (isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0');
+            header($protocol . ' ' . $code . ' ' . $text);
+            $GLOBALS['http_response_code'] = $code;
+        } else {
+            $code = (isset($GLOBALS['http_response_code']) ? $GLOBALS['http_response_code'] : 200);
+            $text = '';
+        }
+        return $text;
+    }
+    /*
+    Generate JWT tokens for authentication
+    Author : Subhomoy
+    */
+    private static function generateToken($userId, $email, $phone){
+        $token      = array(
+            'id'                => $userId,
+            'email'             => $email,
+            'phone'             => $phone,
+            'exp'               => time() + (30 * 24 * 60 * 60) // 30 days
+        );
+        // pr($token);
+        return JWT::encode($token, TOKEN_SECRET, 'HS256');
+    }
+    /*
+    Check Authentication
+    Author : Subhomoy
+    */
+    private function tokenAuth($appAccessToken){
+        $headers = apache_request_headers();
+        if (isset($appAccessToken) && !empty($appAccessToken)) :
+            $userdata = $this->matchToken($appAccessToken);
+            // pr($userdata);
+            if ($userdata['status']) :
+                $checkToken =  UserDevice::where('user_id', '=', $userdata['data']->id)->where('app_access_token', '=', $appAccessToken)->first();
+                // echo $this->db->last_query();
+                // pr($userdata);
+                if (!empty($checkToken)) :
+                    if ($userdata['data']->exp && $userdata['data']->exp > time()) :
+                        $tokenStatus = array(TRUE, $userdata['data']->id, $userdata['data']->email, $userdata['data']->phone, $userdata['data']->exp);
+                    else :
+                        $tokenStatus = array(FALSE, 'Token Has Expired 1 !!!');
+                    endif;
+                else :
+                    $tokenStatus = array(FALSE, 'Token Has Expired 2 !!!');
+                endif;
+            else :
+                $tokenStatus = array(FALSE, 'Token Not Found !!!');
+            endif;
+        else :
+            $tokenStatus = array(FALSE, 'Token Not Found In Request !!!');
+        endif;
+        if ($tokenStatus[0]) :
+            $this->userId           = $tokenStatus[1];
+            $this->userEmail        = $tokenStatus[2];
+            $this->userMobile       = $tokenStatus[3];
+            $this->userExpiry       = $tokenStatus[4];
+            // pr($tokenStatus);
+            return array('status' => TRUE, 'data' => $tokenStatus);
+        else :
+            return array('status' => FALSE, 'data' => $tokenStatus[1]);
+            // $this->response_to_json(FALSE, $tokenStatus[1]);
+        endif;
+    }
+    /*
+    Match JWT token with user token saved in database
+    Author : Subhomoy
+    */
+    private static function matchToken($token){
+        // try{
+        //     // $decoded    = JWT::decode($token, TOKEN_SECRET, 'HS256');
+        //     $decoded    = JWT::decode($token, new Key(TOKEN_SECRET, 'HS256'));
+        //     // pr($decoded);
+        // } catch (\Exception $e) {
+        //     //echo 'Caught exception: ',  $e->getMessage(), "\n";
+        //     return array('status' => FALSE, 'data' => '');
+        // }
+        
+        // return array('status' => TRUE, 'data' => $decoded);
+
+
+        try{
+            $key = "1234567890qwertyuiopmnbvcxzasdfghjkl";
+            $decoded = JWT::decode($token, $key, array('HS256'));
+            // $decodedData = (array) $decoded;
+        } catch (\Exception $e) {
+            //echo 'Caught exception: ',  $e->getMessage(), "\n";
+            return array('status' => FALSE, 'data' => '');
+        }
+        return array('status' => TRUE, 'data' => $decoded);
+    }
 }
