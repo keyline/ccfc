b94c3a7ea76ce198da2a28494cf8fe0ad86b29bf
subhomoy samanta <subhomoy@keylines.net>
2024-06-20 13:22:13 +0530

    my card api implement 20-06-2024

--- app/Http/Controllers/Api/V2/Member/ApiController.php
+++ app/Http/Controllers/Api/V2/Member/ApiController.php
@@ -49,6 +49,10 @@ class ApiController extends Controller
                 $apiStatus          = FALSE;
                 $apiMessage         = 'All Data Are Not Present !!!';
             }
+            echo $headerData['key'][0];
+            echo '<br>';
+            echo env('PROJECT_KEY');
+            Helper::pr($headerData);
             if($headerData['key'][0] == env('PROJECT_KEY')){
                 $phone                      = $requestData['phone'];
                 $device_token               = $requestData['device_token'];
