f36cafe310aa9b82f3e3325731b77cfa55ad04ca
subhomoy samanta <subhomoy@keylines.net>
2024-07-03 14:07:10 +0530

    payuresponse api implemented

--- app/Http/Controllers/Api/V2/Member/ApiController.php
+++ app/Http/Controllers/Api/V2/Member/ApiController.php
@@ -1696,7 +1696,7 @@ class ApiController extends Controller
                                     'body'                  => '',
                                     'destination'           => 'https://ccfc.keylines.in/member/payment/status',
                                     'hash'                  => $hash,
-                                    // 'response'              => $payuResponse,
+                                    'response'              => $payuResponse,
                                     'status'                => (($status == 'failure')?'failed':'successful'),
                                     'created_at'            => date('Y-m-d H:i:s'),
                                     'updated_at'            => date('Y-m-d H:i:s'),
