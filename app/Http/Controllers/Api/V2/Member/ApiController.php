068acd3f979add5121b46cd93eeac981ed864acc
subhomoy samanta <subhomoy@keylines.net>
2024-07-03 18:10:38 +0530

    invoice api issue checking

--- app/Http/Controllers/Api/V2/Member/ApiController.php
+++ app/Http/Controllers/Api/V2/Member/ApiController.php
@@ -1577,7 +1577,7 @@ class ApiController extends Controller
                                             ->withHeaders(['Authorization' => 'Bearer ' . $token, 'Cache-Control' => 'no-cache', 'Accept' => '/',
                                                             'Content-Type' => 'application/json',])
                                             ->withOptions(["verify" => false])
-                                            ->post($tansactionUrl)->json()['data'];
+                                            ->post($tansactionUrl)->json();
                                 Helper::pr($transactions);
 
                                 $monthly_billing                = [];
