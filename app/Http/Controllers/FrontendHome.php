5a5eb437cf042546f522c50eb2e7de47f3263ba4
subhomoy samanta <subhomoy@keylines.net>
2024-07-06 14:07:21 +0530

    api updated on 06-07-2024

--- app/Http/Controllers/FrontendHome.php
+++ app/Http/Controllers/FrontendHome.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use App\Models\ClubmanItem;
 use App\Models\DeleteAccountRequest;
 use App\Models\ReciprocalClub;
