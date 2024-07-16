f7d8abb738b1338b8638b132301158e56987bae7
subhomoy samanta <subhomoy@keylines.net>
2024-07-16 12:59:35 +0530

    api updated on 16-07-2024

--- app/Http/Controllers/Admin/CircularController.php
+++ app/Http/Controllers/Admin/CircularController.php
@@ -4,6 +4,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Models\circular;
+use App\Models\UserDevice;
 
 use Illuminate\Http\Request;
 
--- app/Http/Controllers/Admin/EventsController.php
+++ app/Http/Controllers/Admin/EventsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Models\Events;
+use App\Models\UserDevice;
 
 use Illuminate\Http\Request;
 
