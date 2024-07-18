5d7ed726ae265c29c036796caa31743addb62576
subhomoy samanta <subhomoy@keylines.net>
2024-07-18 13:45:04 +0530

    profile request update implemented 18-07-2024

--- app/Http/Controllers/Api/V2/Member/ApiController.php
+++ app/Http/Controllers/Api/V2/Member/ApiController.php
@@ -2641,7 +2641,7 @@ class ApiController extends Controller
                                             'children3_sex'     => $children3['sex'],
                                         ];
                                     }
-                                    Helper::pr($fields);
+                                    // Helper::pr($fields);
                                     MemberProfileUpdateRequest::insert($fields);
 
                                     $apiStatus          = TRUE;
