c02d77f5d8a94964c34fdec82a2f391b73c964f9
subhomoy samanta <subhomoy@keylines.net>
2024-07-18 14:35:40 +0530

    profile request update implemented 18-07-2024

--- resources/views/email-templates/profile-update-request.blade.php
+++ resources/views/email-templates/profile-update-request.blade.php
@@ -8,6 +8,14 @@ $generalSetting    = GeneralSetting::find(1);
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?=$generalSetting->site_name?></title>
+<style type="text/css">
+    .table-label{
+        border: 1px solid #828282; color: #000000; font-weight: 400; font-size: 14px; font-family: 'Roboto', 'Helvetica Neue', Helvetica, Arial, sans-serif; line-height: 1.5em; margin: 0; padding: 10px;
+    }
+    .table-value{
+        order: 1px solid #828282; color: #000000; font-weight: 600; font-size: 14px; font-family: 'Roboto', 'Helvetica Neue', Helvetica, Arial, sans-serif; line-height: 1.5em; margin: 0; padding: 10px; text-align: left;
+    }
+</style>
 </head>
 <body style="padding: 0; margin: 0;">
   <div style="max-width: 1000px;margin: 0 auto;">
@@ -20,24 +28,27 @@ $generalSetting    = GeneralSetting::find(1);
         <p style="margin-top: 0px;"><?=$member_name?> (<?=$member_code?>) requests profile update </p>
         <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="100%">
             <tr>
-                <td valign="middle" width="16%" style="border: 1px solid #828282; color: #000000; font-weight: 400; font-size: 14px; font-family: 'Roboto', 'Helvetica Neue', Helvetica, Arial, sans-serif; line-height: 1.5em; margin: 0; padding: 10px;"><a href="#" style="color: #000000; text-decoration: none;">Department :</a></td>
-                <td valign="middle" width="28%" style="border: 1px solid #828282; color: #000000; font-weight: 600; font-size: 14px; font-family: 'Roboto', 'Helvetica Neue', Helvetica, Arial, sans-serif; line-height: 1.5em; margin: 0; padding: 10px;"></td> 
-            </tr>
-             <tr>
-                <td valign="middle" width="16%" style="border: 1px solid #828282; color: #000000; font-weight: 400; font-size: 14px; font-family: 'Roboto', 'Helvetica Neue', Helvetica, Arial, sans-serif; line-height: 1.5em; margin: 0; padding: 10px;"><a href="#" style="color: #000000; text-decoration: none;">Name :</a></td>
-                <td  valign="middle" width="28%" style="border: 1px solid #828282; color: #000000; font-weight: 600; font-size: 14px; font-family: 'Roboto', 'Helvetica Neue', Helvetica, Arial, sans-serif; line-height: 1.5em; margin: 0; padding: 10px;"></td> 
-            </tr>
-             <tr>
-                <td valign="middle" width="16%" style="border: 1px solid #828282; color: #000000; font-weight: 400; font-size: 14px; font-family: 'Roboto', 'Helvetica Neue', Helvetica, Arial, sans-serif; line-height: 1.5em; margin: 0; padding: 10px;"><a href="#" style="color: #000000; text-decoration: none;">Email :</a></td>
-                <td valign="middle" width="28%" style="border: 1px solid #828282; color: #000000; font-weight: 600; font-size: 14px; font-family: 'Roboto', 'Helvetica Neue', Helvetica, Arial, sans-serif; line-height: 1.5em; margin: 0; padding: 10px;"></td> 
+                <td valign="middle" width="28%" style="text-align: center;" class="table-value" colspan="2">Member</td> 
             </tr>
             <tr>
-                <td valign="middle" width="16%" style="border: 1px solid #828282; color: #000000; font-weight: 400; font-size: 14px; font-family: 'Roboto', 'Helvetica Neue', Helvetica, Arial, sans-serif; line-height: 1.5em; margin: 0; padding: 10px;"><a href="#" style="color: #000000; text-decoration: none;">Phone :</a></td>
-                <td valign="middle" width="28%" style="border: 1px solid #828282; color: #000000; font-weight: 600; font-size: 14px; font-family: 'Roboto', 'Helvetica Neue', Helvetica, Arial, sans-serif; line-height: 1.5em; margin: 0; padding: 10px; text-align: left;"></td> 
+                <td valign="middle" width="16%" style="" class="table-label"><a href="javascript:void(0);" style="color: #000000; text-decoration: none;">Department :</a></td>
+                <td valign="middle" width="28%" style="" class="table-value"></td> 
+            </tr>
+             <tr>
+                <td valign="middle" width="16%" style="" class="table-label"><a href="javascript:void(0);" style="color: #000000; text-decoration: none;">Name :</a></td>
+                <td  valign="middle" width="28%" style="" class="table-value"></td> 
+            </tr>
+             <tr>
+                <td valign="middle" width="16%" style="" class="table-label"><a href="javascript:void(0);" style="color: #000000; text-decoration: none;">Email :</a></td>
+                <td valign="middle" width="28%" style="" class="table-value"></td> 
             </tr>
             <tr>
-                <td  valign="middle" width="16%" style="border: 1px solid #828282; color: #000000; font-weight: 400; font-size: 14px; font-family: 'Roboto', 'Helvetica Neue', Helvetica, Arial, sans-serif; line-height: 1.5em; margin: 0; padding: 10px;"><a href="#" style="color: #000000; text-decoration: none;">Comment :</a></td>
-                <td  valign="middle" width="28%" style="border: 1px solid #828282; color: #000000; font-weight: 600; font-size: 14px; font-family: 'Roboto', 'Helvetica Neue', Helvetica, Arial, sans-serif; line-height: 1.5em; margin: 0; padding: 10px; text-align: left;"></td> 
+                <td valign="middle" width="16%" style="" class="table-label"><a href="javascript:void(0);" style="color: #000000; text-decoration: none;">Phone :</a></td>
+                <td valign="middle" width="28%" style="" class="table-value"></td> 
+            </tr>
+            <tr>
+                <td  valign="middle" width="16%" style="" class="table-label"><a href="javascript:void(0);" style="color: #000000; text-decoration: none;">Comment :</a></td>
+                <td  valign="middle" width="28%" style="" class="table-value"></td> 
             </tr>
         </table>
         <p style="font-size:0.9em;">Regards,<br />Thanks</p>
