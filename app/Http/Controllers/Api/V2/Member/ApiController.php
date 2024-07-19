da4e0b56290c230b6fe5b638e263e0b578e8e4b0
subhomoy samanta <subhomoy@keylines.net>
2024-07-19 11:44:35 +0530

    api changes on 19-07-2024

--- app/Http/Controllers/Api/V2/Member/ApiController.php
+++ app/Http/Controllers/Api/V2/Member/ApiController.php
@@ -787,16 +787,37 @@ class ApiController extends Controller
                                   ]
                                 );
 
-                                $token = "5tdpn6yeoycRKbWd0311m1B5S-ZKMfU2syAD50kiquOX20GbmXF89Z1-vvsN01WTAIRWHdRESd8nRWZJrC7xuHkClh63BPg1PCpZHKpDOjmtvgJL8ErYrup7PLG2LZHkbjDh6bFb54VyUsvZm4OzzIPI9QVKhTf2ui5Pmd8CzHJZUK-4Jd-aOmQFfhuertA5KuIRrNdHTzA7w1hEYHO9Hq9J_pkME7BhNpjWp44Z3R2YeLuQbskl_rMypzLj5icdoPWgCsxA1bU9iGo5x3heaP8lHliiSx3SeeYpBMe22DRaarXJYc5pxFJ1tuEKDoxn";
-                                $qrcodeFields = [
-                                    'mcode'     => $checkUser->user_code
-                                ];
-                                $qrcodeUrl    = 'https://ccfcmemberdata.in/Api/CardInfo/POST?' . http_build_query($qrcodeFields);
-                                $qrcodes      = Http::withoutVerifying()
-                                            ->withHeaders(['Authorization' => 'Bearer ' . $token, 'Cache-Control' => 'no-cache', 'Accept' => '/',
-                                                            'Content-Type' => 'application/json',])
-                                            ->withOptions(["verify" => false])
-                                            ->post($qrcodeUrl)->json()['data'];
+                                // $token = "5tdpn6yeoycRKbWd0311m1B5S-ZKMfU2syAD50kiquOX20GbmXF89Z1-vvsN01WTAIRWHdRESd8nRWZJrC7xuHkClh63BPg1PCpZHKpDOjmtvgJL8ErYrup7PLG2LZHkbjDh6bFb54VyUsvZm4OzzIPI9QVKhTf2ui5Pmd8CzHJZUK-4Jd-aOmQFfhuertA5KuIRrNdHTzA7w1hEYHO9Hq9J_pkME7BhNpjWp44Z3R2YeLuQbskl_rMypzLj5icdoPWgCsxA1bU9iGo5x3heaP8lHliiSx3SeeYpBMe22DRaarXJYc5pxFJ1tuEKDoxn";
+                                // $qrcodeFields = [
+                                //     'mcode'     => $checkUser->user_code
+                                // ];
+                                // $qrcodeUrl    = 'https://ccfcmemberdata.in/Api/CardInfo/POST?' . http_build_query($qrcodeFields);
+                                // $qrcodes      = Http::withoutVerifying()
+                                //             ->withHeaders(['Authorization' => 'Bearer ' . $token, 'Cache-Control' => 'no-cache', 'Accept' => '/',
+                                //                             'Content-Type' => 'application/json',])
+                                //             ->withOptions(["verify" => false])
+                                //             ->post($qrcodeUrl)->json()['data'];
+
+                                $url = "https://ccfcmemberdata.in/Api/CardInfo/POST?mcode=" . $checkUser->user_code;
+                                $curl = curl_init($url);
+                                curl_setopt($curl, CURLOPT_URL, $url);
+                                curl_setopt($curl, CURLOPT_POST, true);
+                                curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+                                $headers = array(
+                                   "Authorization: Bearer 5tdpn6yeoycRKbWd0311m1B5S-ZKMfU2syAD50kiquOX20GbmXF89Z1-vvsN01WTAIRWHdRESd8nRWZJrC7xuHkClh63BPg1PCpZHKpDOjmtvgJL8ErYrup7PLG2LZHkbjDh6bFb54VyUsvZm4OzzIPI9QVKhTf2ui5Pmd8CzHJZUK-4Jd-aOmQFfhuertA5KuIRrNdHTzA7w1hEYHO9Hq9J_pkME7BhNpjWp44Z3R2YeLuQbskl_rMypzLj5icdoPWgCsxA1bU9iGo5x3heaP8lHliiSx3SeeYpBMe22DRaarXJYc5pxFJ1tuEKDoxn",
+                                   "Content-Type: application/json",
+                                   "Content-Length: 0",
+                                );
+                                curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+                                //for debug only!
+                                curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+                                curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+                                $resp = curl_exec($curl);
+                                // curl_close($curl);
+                                // Helper::pr($resp);
+                                $qrcodes = json_decode($resp)['data'];
+
                                 $SIXTEEN_DIGIT_CODE = '';
                                 if($qrcodes){
                                     $SIXTEEN_DIGIT_CODE = $qrcodes[0]['16_DIGIT_CODE'];
