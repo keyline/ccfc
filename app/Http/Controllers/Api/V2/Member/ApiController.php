4d73aaea72e90e3974ee31b284026b014c386285
subhomoy samanta <subhomoy@keylines.net>
2024-07-23 11:13:47 +0530

    api implemented on 23-07-2024

--- app/Http/Controllers/Api/V2/Member/ApiController.php
+++ app/Http/Controllers/Api/V2/Member/ApiController.php
@@ -1941,51 +1941,26 @@ class ApiController extends Controller
                         if($checkUser){
                             if($checkUser->status == 'ACTIVE'){
                                 // // $token = "N3bwPrgB4wzHytcBkrvd6duSAX46ksfh9zOGPGnzwL8YladUpD-XH0DD_ZVBfdktfuPvgMbHg4uvBNBzibf2qEvPWh-HlzMFwnWJCfI8uW7-RBbpBj5oPlL9KPj7jxL8kaHDB6Fvl1fc8KZfYpZlRKRRTXIqsOkWt4Wenzz8I-D42AQzY5u-4FF1lDN3pepkwSL6xxXEb6wHExSHYlqT_9mKOB-6P-h6uWeqLETbFnft0CBvzwo9rJ14Gvu1YesR_Yte88Xg9R1K4_2mlY93YxYJGI7I3LkPSsVBfPW1SkzmdWo3HRJci6nRl36U_Llc";
-                                // $token = "5tdpn6yeoycRKbWd0311m1B5S-ZKMfU2syAD50kiquOX20GbmXF89Z1-vvsN01WTAIRWHdRESd8nRWZJrC7xuHkClh63BPg1PCpZHKpDOjmtvgJL8ErYrup7PLG2LZHkbjDh6bFb54VyUsvZm4OzzIPI9QVKhTf2ui5Pmd8CzHJZUK-4Jd-aOmQFfhuertA5KuIRrNdHTzA7w1hEYHO9Hq9J_pkME7BhNpjWp44Z3R2YeLuQbskl_rMypzLj5icdoPWgCsxA1bU9iGo5x3heaP8lHliiSx3SeeYpBMe22DRaarXJYc5pxFJ1tuEKDoxn";
-                                // // $fields = [
-                                // //     'MCODE' => $checkUser->user_code
-                                // // ];
-                                // // $url = "https://ccfcmemberdata.in/Api/MemberProfile/?".http_build_query($fields);
-                                // $transactionFields = [
-                                //     'MCODE'     => $checkUser->user_code,
-                                //     'FromDate'  => '01-apr-2020',
-                                //     'ToDate'    => '01-jun-2021',
+                                $token = "5tdpn6yeoycRKbWd0311m1B5S-ZKMfU2syAD50kiquOX20GbmXF89Z1-vvsN01WTAIRWHdRESd8nRWZJrC7xuHkClh63BPg1PCpZHKpDOjmtvgJL8ErYrup7PLG2LZHkbjDh6bFb54VyUsvZm4OzzIPI9QVKhTf2ui5Pmd8CzHJZUK-4Jd-aOmQFfhuertA5KuIRrNdHTzA7w1hEYHO9Hq9J_pkME7BhNpjWp44Z3R2YeLuQbskl_rMypzLj5icdoPWgCsxA1bU9iGo5x3heaP8lHliiSx3SeeYpBMe22DRaarXJYc5pxFJ1tuEKDoxn";
+                                // $fields = [
+                                //     'MCODE' => $checkUser->user_code
                                 // ];
+                                // $url = "https://ccfcmemberdata.in/Api/MemberProfile/?".http_build_query($fields);
+                                $transactionFields = [
+                                    'MCODE'     => $checkUser->user_code,
+                                    'FromDate'  => '01-apr-2020',
+                                    'ToDate'    => '01-jun-2021',
+                                ];
 
-                                // $tansactionUrl = 'https://ccfcmemberdata.in/api/MemberMonthlyBalance/?' . http_build_query($transactionFields);
-                                // $transactions = Http::withoutVerifying()
-                                //             ->withHeaders(['Authorization' => 'Bearer ' . $token, 'Cache-Control' => 'no-cache', 'Accept' => '/',
-                                //                             'Content-Type' => 'application/json',])
-                                //             ->withOptions(["verify" => false])
-                                //             ->post($tansactionUrl)->json()['data'];
-                                // // Helper::pr($transactions);
+                                $tansactionUrl = 'https://ccfcmemberdata.in/api/MemberMonthlyBalance/?' . http_build_query($transactionFields);
+                                $transactions = Http::withoutVerifying()
+                                            ->withHeaders(['Authorization' => 'Bearer ' . $token, 'Cache-Control' => 'no-cache', 'Accept' => '/',
+                                                            'Content-Type' => 'application/json',])
+                                            ->withOptions(["verify" => false])
+                                            ->post($tansactionUrl)->json()['data'];
+                                Helper::pr($transactions);die;
 
-                                $curl = curl_init();
-
-                                curl_setopt_array($curl, array(
-                                  CURLOPT_URL => 'https://ccfc.keylines.in/api/v2/member/billing',
-                                  CURLOPT_RETURNTRANSFER => true,
-                                  CURLOPT_ENCODING => '',
-                                  CURLOPT_MAXREDIRS => 10,
-                                  CURLOPT_TIMEOUT => 0,
-                                  CURLOPT_FOLLOWLOCATION => true,
-                                  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                                  CURLOPT_CUSTOMREQUEST => 'GET',
-                                  CURLOPT_HTTPHEADER => array(
-                                    'Key: facb6e0a6fcbe200dca2fb60dec75be7',
-                                    'source: ANDROID',
-                                    'Authorization: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NDM3MywiZW1haWwiOiJkZWJ0ZWpAZ21haWwuY29tIiwicGhvbmUiOiI5ODMxMDU1NDAwIiwiZXhwIjoxNzI0MzAxNzA4fQ.WhAAbfPxsSOMPqmAEDjbKfKC5WVNMkcz7O1c1lKggpg',
-                                    'Cache-Control: no-cache',
-                                    'Pragma: no-cache',
-                                    'Content-Type: application/json; charset=utf-8',
-                                    'Expires: -1'
-                                  ),
-                                ));
-
-                                $response = curl_exec($curl);
-
-                                curl_close($curl);
-                                Helper::pr($response);die;
+                                
 
                                 $url = "https://ccfcmemberdata.in/api/MemberMonthlyBalance/?MCODE=" . $checkUser->user_code . "&FromDate=01-apr-2020&ToDate=01-jun-2021";
                                 $curl = curl_init($url);
