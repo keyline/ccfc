b8cc663cc9a67adf1b5ac4bb59519071528abbe9
subhomoy samanta <subhomoy@keylines.net>
2024-07-23 12:05:08 +0530

    api implemented on 23-07-2024

--- app/Http/Controllers/Api/V2/Member/ApiController.php
+++ app/Http/Controllers/Api/V2/Member/ApiController.php
@@ -790,33 +790,28 @@ class ApiController extends Controller
                                 );
 
                                 // $token = "5tdpn6yeoycRKbWd0311m1B5S-ZKMfU2syAD50kiquOX20GbmXF89Z1-vvsN01WTAIRWHdRESd8nRWZJrC7xuHkClh63BPg1PCpZHKpDOjmtvgJL8ErYrup7PLG2LZHkbjDh6bFb54VyUsvZm4OzzIPI9QVKhTf2ui5Pmd8CzHJZUK-4Jd-aOmQFfhuertA5KuIRrNdHTzA7w1hEYHO9Hq9J_pkME7BhNpjWp44Z3R2YeLuQbskl_rMypzLj5icdoPWgCsxA1bU9iGo5x3heaP8lHliiSx3SeeYpBMe22DRaarXJYc5pxFJ1tuEKDoxn";
-                                // $qrcodeFields = [
-                                //     'mcode'     => $checkUser->user_code
-                                // ];
-                                // $qrcodeUrl    = 'https://ccfcmemberdata.in/Api/CardInfo/POST?' . http_build_query($qrcodeFields);
-                                // $qrcodes      = Http::withoutVerifying()
-                                //             ->withHeaders(['Authorization' => 'Bearer ' . $token, 'Cache-Control' => 'no-cache', 'Accept' => '/',
-                                //                             'Content-Type' => 'application/json',])
-                                //             ->withOptions(["verify" => false])
-                                //             ->post($qrcodeUrl)->json()['data'];
+                                $url            = "https://ccfcmemberdata.in/Api/CardInfo/POST?mcode=" . $checkUser->user_code;;
+                                $postData       = ['mcode' => $checkUser->user_code];
+                                $response       = $this->makeCurlRequest($url, $postData);
+                                $qrcodes        = json_decode($resp, true)['data'];
 
-                                $url = "https://ccfcmemberdata.in/Api/CardInfo/POST?mcode=" . $checkUser->user_code;
-                                $curl = curl_init($url);
-                                curl_setopt($curl, CURLOPT_URL, $url);
-                                curl_setopt($curl, CURLOPT_POST, true);
-                                curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-                                $headers = array(
-                                   "Authorization: Bearer 5tdpn6yeoycRKbWd0311m1B5S-ZKMfU2syAD50kiquOX20GbmXF89Z1-vvsN01WTAIRWHdRESd8nRWZJrC7xuHkClh63BPg1PCpZHKpDOjmtvgJL8ErYrup7PLG2LZHkbjDh6bFb54VyUsvZm4OzzIPI9QVKhTf2ui5Pmd8CzHJZUK-4Jd-aOmQFfhuertA5KuIRrNdHTzA7w1hEYHO9Hq9J_pkME7BhNpjWp44Z3R2YeLuQbskl_rMypzLj5icdoPWgCsxA1bU9iGo5x3heaP8lHliiSx3SeeYpBMe22DRaarXJYc5pxFJ1tuEKDoxn",
-                                   "Content-Type: application/json",
-                                   "Content-Length: 0",
-                                );
-                                curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-                                //for debug only!
-                                curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-                                curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+                                // $url = "https://ccfcmemberdata.in/Api/CardInfo/POST?mcode=" . $checkUser->user_code;
+                                // $curl = curl_init($url);
+                                // curl_setopt($curl, CURLOPT_URL, $url);
+                                // curl_setopt($curl, CURLOPT_POST, true);
+                                // curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+                                // $headers = array(
+                                //    "Authorization: Bearer 5tdpn6yeoycRKbWd0311m1B5S-ZKMfU2syAD50kiquOX20GbmXF89Z1-vvsN01WTAIRWHdRESd8nRWZJrC7xuHkClh63BPg1PCpZHKpDOjmtvgJL8ErYrup7PLG2LZHkbjDh6bFb54VyUsvZm4OzzIPI9QVKhTf2ui5Pmd8CzHJZUK-4Jd-aOmQFfhuertA5KuIRrNdHTzA7w1hEYHO9Hq9J_pkME7BhNpjWp44Z3R2YeLuQbskl_rMypzLj5icdoPWgCsxA1bU9iGo5x3heaP8lHliiSx3SeeYpBMe22DRaarXJYc5pxFJ1tuEKDoxn",
+                                //    "Content-Type: application/json",
+                                //    "Content-Length: 0",
+                                // );
+                                // curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+                                // //for debug only!
+                                // curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+                                // curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
-                                $resp = curl_exec($curl);
-                                $qrcodes = json_decode($resp, true)['data'];
+                                // $resp = curl_exec($curl);
+                                // $qrcodes = json_decode($resp, true)['data'];
 
                                 $SIXTEEN_DIGIT_CODE = '';
                                 if($qrcodes){
@@ -2347,7 +2342,7 @@ class ApiController extends Controller
                 // Set cURL options
                 curl_setopt($ch, CURLOPT_URL, $url);
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-                curl_setopt($ch, CURLOPT_TIMEOUT, 10); // Set a timeout
+                curl_setopt($ch, CURLOPT_TIMEOUT, 100); // Set a timeout
 
                 // If POST data is provided, make a POST request
                 if (!empty($postData)) {
