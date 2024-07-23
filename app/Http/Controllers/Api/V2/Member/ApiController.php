acf32cbd701b62bffc40dbc62a51e783e279ca7b
subhomoy samanta <subhomoy@keylines.net>
2024-07-23 12:13:16 +0530

    api implemented on 23-07-2024

--- app/Http/Controllers/Api/V2/Member/ApiController.php
+++ app/Http/Controllers/Api/V2/Member/ApiController.php
@@ -2353,6 +2353,7 @@ class ApiController extends Controller
                    "Authorization: Bearer 5tdpn6yeoycRKbWd0311m1B5S-ZKMfU2syAD50kiquOX20GbmXF89Z1-vvsN01WTAIRWHdRESd8nRWZJrC7xuHkClh63BPg1PCpZHKpDOjmtvgJL8ErYrup7PLG2LZHkbjDh6bFb54VyUsvZm4OzzIPI9QVKhTf2ui5Pmd8CzHJZUK-4Jd-aOmQFfhuertA5KuIRrNdHTzA7w1hEYHO9Hq9J_pkME7BhNpjWp44Z3R2YeLuQbskl_rMypzLj5icdoPWgCsxA1bU9iGo5x3heaP8lHliiSx3SeeYpBMe22DRaarXJYc5pxFJ1tuEKDoxn",
                    "Content-Type: application/json",
                    "Content-Length: 0",
+                   "Cache-Control: no-cache",
                 );
                 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
